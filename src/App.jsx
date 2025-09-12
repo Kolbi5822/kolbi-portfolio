e60555a78ff5f53df7a5023e2ecfd250003fcc41
Kolbi Lee <kolbi5822@gmail.com>
2025-09-11 22:37:46 -0400

    Portfolio partial

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import './styles/Navbar.css';
-
-function Page({ color }) {
-  return (
-    <div
-      style={{
-        backgroundColor: color,
-        width: '100vw',
-        height: '100vh',
-        paddingTop: 'var(--navbar-height)',
-      }}
-    />
-  );
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="kolbi-portfolio/" element={<Page color="#4281A4" />} />
-        <Route path="kolbi-portfolio/page2" element={<Page color="#48A9A6" />} />
-        <Route path="kolbi-portfolio/page3" element={<Page color="#E4DFDA" />} />
-        <Route path="kolbi-portfolio/page4" element={<Page color="#D4B483" />} />
-        <Route path="kolbi-portfolio/page5" element={<Page color="#C1666B" />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/kolbi-portfolio" index element={<Home />}/>
+          <Route path="*" element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
